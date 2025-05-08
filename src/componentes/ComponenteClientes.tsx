63dbae4eeade179b5435bfc71dbf45bba8a3a972
Marlon Lopez <86318634+Marlon599@users.noreply.github.com>
2025-05-08 16:46:08 -0600

    Update ComponenteClientes.tsx

--- src/componentes/ComponenteClientes.tsx
+++ src/componentes/ComponenteClientes.tsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react";
 import { jsPDF } from "jspdf";
-import { obtenerClientes } from "../Services/ServiceCliente";
+import { obtenerClientes } from "../Services/ServiceCliente.ts";
+import autoTable from "jspdf-autotable";
 
+// Adaptado a la estructura real del backend
 interface Cliente {
   carnet: string;
   nombre: string;
@@ -21,35 +23,54 @@ const ComponenteClientes = () => {
     fetchClientes();
   }, []);
 
-  // Función para generar PDF
+const logoURL =
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Escudo_de_la_universidad_Mariano_G%C3%A1lvez_Guatemala.svg/512px-Escudo_de_la_universidad_Mariano_G%C3%A1lvez_Guatemala.svg.png";
+  const marcaAguaURL =
+    "https://assets.isu.pub/document-structure/221119120331-2636df8d77a0399b11446057db0bdd7d/v1/ee86784e8c89885cab00d66e46522eaf.jpeg";
+
   const generarPDF = () => {
     const doc = new jsPDF();
-    doc.text("Lista de Clientes", 10, 10);
+    const pageWidth = doc.internal.pageSize.getWidth();
+    const pageHeight = doc.internal.pageSize.getHeight();
+    doc.addImage(logoURL, "JPEG", pageWidth - 30, 10, 20, 20);
+    doc.addImage(marcaAguaURL, "JPEG", pageWidth / 2 - 50, pageHeight / 2 - 50, 100, 100);
 
-    let y = 20;
-    doc.text("Carnet  Nombre  Apellido  Dirección  Carrera", 10, y);
-    y += 10;
+    doc.setFontSize(18);
+    doc.text("Lista de Pagos", pageWidth / 2, 20, { align: "center" });
 
-    clientes.forEach((cliente) => {
-      doc.text(
-        `${cliente.carnet}  ${cliente.nombre}  ${cliente.apellido}  ${cliente.direccion}  ${cliente.carrera}`,
-        10,
-        y
-      );
-      y += 10;
+    const startY = 40;
+    doc.setFontSize(18);
+    doc.text("Lista de Clientes", pageWidth / 2, 20, { align: "center" });
+
+    const encabezados = [["Carnet", "Nombre", "Apellido", "Dirección", "Carrera"]];
+    const datos = clientes.map((cliente) => [
+      cliente.carnet,
+      cliente.nombre,
+      cliente.apellido,
+      cliente.direccion,
+      cliente.carrera,
+    ]);
+
+    autoTable(doc, {
+      startY: 40,
+      head: encabezados,
+      body: datos,
+      styles: { fontSize: 10 },
+      headStyles: { fillColor: "#704a35", textColor: "#ffffff", halign: "center" },
+      alternateRowStyles: { fillColor: "#edbc8b" },
+      theme: "striped",
     });
 
     doc.save("Clientes.pdf");
   };
 
-  // URL del Reporte de Power BI (Reemplaza con la URL real)
   const generarReportePowerBI = () => {
     window.open("https://app.powerbi.com/view?r=tu_reporte_id", "_blank");
   };
 
   return (
     <div>
-      <h2>Lista de Clientes</h2>
+      <h2>Listado de Clientes</h2>
       <table border={1} width="100%">
         <thead>
           <tr style={{ backgroundColor: "lightblue" }}>
