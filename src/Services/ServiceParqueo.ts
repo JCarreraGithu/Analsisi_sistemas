7462ef11a8097976d291b6c07f8d9846a3c2c5a6
JulioXol <andrexol2002@gmail.com>
2025-05-08 22:28:01 -0600

    modifico el pdf parqueos

--- src/Services/ServiceParqueo.ts
+++ src/Services/ServiceParqueo.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/parqueos"; // Ajusta la URL según tu backend
+const API_URL = "http://localhost:3000/api/disponibilidad_parqueo"; // Ajusta la URL según tu backend
 
 export const obtenerParqueos = async () => {
   try {
